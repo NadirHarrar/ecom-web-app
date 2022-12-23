c2be5c1546b619afe7f06a13061fc9e6e3c93291
Nadirouss <nadirharrar71@gmail.com>
2022-12-23 21:57:05 +0100

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,55 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER,NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import {HttpClientModule} from "@angular/common/http";
+import { CustomersComponent } from './customers/customers.component';
+import { OrdersComponent} from "./orders/orders.component";
+import { OrderDetailsComponent } from './order-details/order-details.component';
+import {KeycloakAngularModule,KeycloakService} from "keycloak-angular";
+
+/*function initializedKeycloak(keycloak: KeycloakService){
+  return () =>
+    keycloak.init({
+      config: {
+        url:'http://localhost:8080/auth',
+        realm:'my-ecom-realm',
+        clientId:'ecom-web',
+      },
+      initOptions:{
+        onLoad:'check-sso',
+        silentCheckSsoRedirectUri:
+          window.location.origin + ''
+      },
+    });
+}
+*/
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    CustomersComponent,
+    OrdersComponent,
+    OrderDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    KeycloakAngularModule
   ],
-  providers: [],
+
+  providers: [
+   /* {
+      provide: APP_INITIALIZER,
+      useFactory: initializedKeycloak,
+      multi:true,
+      deps: [KeycloakService]
+    }*/
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
